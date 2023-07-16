e8888663a7344652143270973380a3516806b7e8
Aldo Mardel <mardelaldo@gmail.com>
2023-07-16 16:53:44 -0600

    Agregando pagina payment y pasarela con stripe

--- Cliente/src/Pages/Payment/Payment.jsx
+++ Cliente/src/Pages/Payment/Payment.jsx
@@ -1,9 +1,34 @@
-
+import { FaCreditCard, FaCcPaypal } from "react-icons/fa";
+import './Payment.scss';
 
 const Payment = () => {
 
     return(
-        <h1>Payment</h1>
+        <div className="payment__container">
+        <h1>Pago</h1>
+        <p className="payment__subtitle">Elige el método de pago que mas prefieras.</p>
+
+        <div className="payment__types-container">
+            <a href="https://buy.stripe.com/test_fZe6qq37UatA6je288">
+                <div className="payment__types-stripe">
+                    <div className="payment__types-stripe_icon">
+                        <FaCreditCard/>
+                    </div>
+                    <p>Pago con tarjeta de Crédito/Débito</p>
+                </div>
+            </a>
+
+            <a href="#">
+                <div className="payment__types-paypal">
+                    <div className="payment__types-stripe_icon">
+                        <FaCcPaypal/>
+                    </div>
+                    <p>Pago con Paypal</p>
+                </div>
+            </a>
+
+        </div>
+    </div>
     )
 }
 
